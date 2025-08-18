a9658e371048bf541b5b34636e2d06b2935f1fbf
Stephen Mangum <stephenmangum01@gmail.com>
2025-08-18 10:46:21 -0500

    Starting off

---
+++ References_Mangum_Stephen/IAutomobile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace References_Mangum_Stephen
+{
+    internal interface IAutomobile
+    {
+    }
+}
---
+++ References_Mangum_Stephen/Sedan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace References_Mangum_Stephen // Continue video at 4:22
+{
+    internal class Sedan : IAutomobile
+    {
+        public double Speed => throw new NotImplementedException();
+
+        public int Wheels => throw new NotImplementedException();
+
+        public string LicensePlate => throw new NotImplementedException();
+
+        public void Stringify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
