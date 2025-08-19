4ae54bf21c9383f4852a6f101934ad2de01afbcc
Stephen Mangum <stephenmangum01@gmail.com>
2025-08-19 08:52:19 -0500

    Add Truck class and update Program to use it
    Introduced a new Truck class implementing IAutomobile with properties for speed, weight, wheels, and license plate. Updated Program.cs to create and use Truck and Sedan instances, demonstrating their methods and interactions.

---
+++ References_Mangum_Stephen/Truck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace References_Mangum_Stephen
+{
+    internal class Truck : IAutomobile
+    {
+        public double Speed { get; private set; }
+
+        public int Wheels { get; private set; }
+
+        public string LicensePlate { get; private set; }
+
+        public double weight { get; }
+
+        public Truck(double speedParam, double weightParam, string licenseNum)
+        {
+            Speed = speedParam;
+            weight = weightParam;
+            LicensePlate = licenseNum;
+
+            if(weight < 400)
+            {
+                Wheels = 8;
+            }
+            else
+            {
+                Wheels = 12;
+            }
+        }
+
+
+        public void Stringify()
+        {
+            Console.WriteLine($"The truck is traveling at a speed of {Speed} on {Wheels} wheels with a License Plate # of {LicensePlate}");
+        }
+        public void IncreaseSpeed()
+        {
+            Speed += 5;
+        }
+
+        public void DecreaseSpeed()
+        {
+            Speed -= 5;
+        }
+    }
+}
